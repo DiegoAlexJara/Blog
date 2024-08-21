46e4d8f8988177dcfe911c4beee2460c5806d1c4
DiegoAlexJara <131843241+DiegoAlexJara@users.noreply.github.com>
2024-08-20 18:29:27 -0600

    migrate

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>INICIO</title>
-</head>
-<body>
+@extends('components.app-layout')
+@section('title')
+INICIO
+@endsection
+@section('content')
 
-    <h1>BIENVENIDO COMPAÑERO</h1>
     
-</body>
-</html>
+
+@endsection
